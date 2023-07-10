4a64760ebb7e84b91fb7776741cad9d04f849fdf
DanielGabrys <daniello.gab@gmail.com>
2023-07-10 12:19:50 +0200

    refactoring migrations

--- database/migrations/2023_03_30_130429_create_song_table.php
+++ database/migrations/2023_03_30_130429_create_song_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('title');
-            $table->string('author')->unique();
+            $table->string('author');
             $table->string('image')->nullable();
             $table->string('src');
 
--- database/migrations/2023_04_02_133911_create_playlist_table.php
+++ database/migrations/2023_04_02_133911_create_playlist_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('playlist', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->Text('description')->nullable();
             $table->string('image')->nullable();
 
