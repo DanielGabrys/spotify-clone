6062f24bb2b6561b1601d0d4287a8d34ad9bfa98
DanielGabrys <daniello.gab@gmail.com>
2023-07-12 10:50:24 +0200

    refactoring code

--- database/migrations/2023_05_29_120434_create_tag_template_pivot_table.php
+++ database/migrations/2023_05_29_120434_create_tag_template_pivot_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('track_template_pivot', function (Blueprint $table) {
+        Schema::create('tag_template_pivot', function (Blueprint $table) {
             $table->id();
 
             $table->bigInteger("template_id")->unsigned();
